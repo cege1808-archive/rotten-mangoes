6915c4422e5e507db0a5f6a4af9e1eea06376e88
cege1808 <gunawan.clarissa@hotmail.com>
2016-07-20 20:43:07 +0000

    add admin routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,13 @@ Rails.application.routes.draw do
   resources :movies do 
     resources :reviews, only: [:new, :create]
   end
+
+  namespace :admin do
+    resources :users, only: [:index, :new, :create]
+  end
+
   resources :users, only: [:new, :create]
+  
   resources :sessions, only: [:new, :create, :destroy]
 
   root to: 'movies#index'
